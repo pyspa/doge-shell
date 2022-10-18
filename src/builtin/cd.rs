229b41b377d8b7d192af8e8ad854ae4d82685f3d
mopemope <yutaka.matsubara@gmail.com>
2022-10-18 13:27:23 +0900

    Fix lock stdio

--- src/builtin/cd.rs
+++ src/builtin/cd.rs
@@ -3,6 +3,7 @@ use crate::shell::Shell;
 use dirs;
 use std::fs::File;
 use std::io::Write;
+use std::mem;
 use std::os::unix::io::FromRawFd;
 use std::path::Path;
 
@@ -12,12 +13,19 @@ pub fn command(ctx: &Context, argv: Vec<String>, shell: &mut Shell) -> ExitStatu
     let dir = match argv.get(1).map(|s| s.as_str()) {
         Some(dir) if dir.starts_with('/') => dir.to_string(),
         Some(dir) if dir.starts_with('~') => shellexpand::tilde(dir).to_string(),
-        Some(dir) => Path::new(&current_dir)
-            .join(dir.to_string())
-            .canonicalize()
-            .unwrap() // TODO error check
-            .to_string_lossy()
-            .into_owned(),
+        Some(dir) => {
+            let res = Path::new(&current_dir).join(dir.to_string()).canonicalize();
+
+            match res {
+                Ok(res) => res.to_string_lossy().into_owned(),
+                Err(err) => {
+                    let file = unsafe { File::from_raw_fd(ctx.outfile) };
+                    writeln!(&file, "cd: {}: {}", err, dir).ok();
+                    mem::forget(file);
+                    return ExitStatus::ExitedWith(1);
+                }
+            }
+        }
         None => {
             if let Some(home_dir) = dirs::home_dir() {
                 home_dir.to_string_lossy().into_owned()
@@ -36,8 +44,9 @@ pub fn command(ctx: &Context, argv: Vec<String>, shell: &mut Shell) -> ExitStatu
             ExitStatus::ExitedWith(0)
         }
         Err(err) => {
-            let mut file = unsafe { File::from_raw_fd(ctx.outfile) };
-            writeln!(file, "cd: {}: `{}'", err, dir).ok();
+            let file = unsafe { File::from_raw_fd(ctx.outfile) };
+            writeln!(&file, "cd: {}: {}", err, dir).ok();
+            mem::forget(file);
             ExitStatus::ExitedWith(1)
         }
     }

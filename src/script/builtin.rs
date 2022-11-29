81f7441647dce8ac3eb6a726199048df5d2a0741
mopeomope <yutaka.matsubara@gmail.com>
2022-11-29 15:38:23 +0900

    Add command func

--- src/script/builtin.rs
+++ src/script/builtin.rs
@@ -3,6 +3,7 @@ use rust_lisp::default_env;
 use rust_lisp::model::{Env, ForeignValue, RuntimeError, Symbol, Value};
 use rust_lisp::utils::require_typed_arg;
 use std::convert::From;
+use std::process::Command;
 use std::{cell::RefCell, rc::Rc};
 
 #[derive(Debug)]
@@ -58,15 +59,62 @@ pub fn make_env(config: Rc<RefCell<config::Config>>) -> Rc<RefCell<Env>> {
 
     // add builtin functions
     env.borrow_mut()
+        .define(Symbol::from("debug"), Value::NativeFunc(print));
+    env.borrow_mut()
+        .define(Symbol::from("print"), Value::NativeFunc(print));
+    env.borrow_mut()
         .define(Symbol::from("alias"), Value::NativeFunc(alias));
+    env.borrow_mut()
+        .define(Symbol::from("command"), Value::NativeFunc(command));
     // TODO add shell env
     env
 }
 
-pub fn alias(env: Rc<RefCell<Env>>, args: &[Value]) -> Result<Value, RuntimeError> {
+fn print(_env: Rc<RefCell<Env>>, args: &[Value]) -> Result<Value, RuntimeError> {
+    args.iter().for_each(|x| println!("print: {:?}", x));
+    Ok(Value::True)
+}
+
+fn alias(env: Rc<RefCell<Env>>, args: &[Value]) -> Result<Value, RuntimeError> {
     if let Some(Value::Foreign(val)) = env.borrow_mut().get(&Symbol::from("config")) {
         val.borrow_mut().command(env.clone(), "set-alias", args)
     } else {
         Ok(Value::NIL)
+    }
+}
+
+fn command(_env: Rc<RefCell<Env>>, args: &[Value]) -> Result<Value, RuntimeError> {
+    let mut cmd_args: Vec<String> = Vec::new();
+    for arg in args {
+        if let Value::String(val) = arg {
+            val.split_whitespace()
+                .for_each(|x| cmd_args.push(x.to_string()));
+        }
+    }
+
+    let cmd = cmd_args.remove(0);
+
+    match Command::new(cmd).args(cmd_args).output() {
+        Ok(output) => {
+            if output.status.success() {
+                let stdout = String::from_utf8(output.stdout).expect("fail get stdout");
+
+                // let mut out: Vec<String> = Vec::new();
+                // stdout.split('\n').for_each(|x| out.push(x.to_string()));
+                // match make_list(out) {
+                //     Ok(lst) => Ok(lst),
+                //     Err(err) => Err(RuntimeError {
+                //         msg: err.to_string(),
+                //     }),
+                // }
+
+                Ok(Value::String(stdout))
+            } else {
+                Ok(Value::NIL)
+            }
+        }
+        Err(err) => Err(RuntimeError {
+            msg: err.to_string(),
+        }),
     }
 }

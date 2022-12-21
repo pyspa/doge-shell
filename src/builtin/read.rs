8569c356943e50a5c80cab9e9ce08203ea6761ce
mopeomope <yutaka.matsubara@gmail.com>
2022-12-22 08:12:15 +0900

    Fix key prefix

--- src/builtin/read.rs
+++ src/builtin/read.rs
@@ -7,7 +7,7 @@ use std::os::unix::io::FromRawFd;
 pub fn command(ctx: &Context, argv: Vec<String>, shell: &mut Shell) -> ExitStatus {
     let mut stdin = Vec::new();
     unsafe { File::from_raw_fd(ctx.infile).read_to_end(&mut stdin).ok() };
-    let key = format!("${}", argv[1]);
+    let key = format!("{}", argv[1]);
     let output = std::str::from_utf8(&stdin)
         .map_err(|err| {
             // TODO

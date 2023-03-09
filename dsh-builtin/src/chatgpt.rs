dab7b6980d26e5548eefd1badf65f170760e0ffa
mopemope <yutaka.matsubara@gmail.com>
2023-03-09 19:45:20 +0900

    Set skin

--- dsh-builtin/src/chatgpt.rs
+++ dsh-builtin/src/chatgpt.rs
@@ -1,6 +1,8 @@
 use super::ShellProxy;
 use dsh_chatgpt::ChatGptClient;
 use dsh_types::{Context, ExitStatus};
+use termimad::crossterm::style::Color;
+use termimad::MadSkin;
 
 const PROMPT_KEY: &str = "CHAT_PROMPT";
 
@@ -10,9 +12,14 @@ pub fn chat(_ctx: &Context, argv: Vec<String>, proxy: &mut dyn ShellProxy) -> Ex
             Ok(client) => {
                 let content = &argv[1];
                 let prompt = proxy.get_var(PROMPT_KEY);
-                match client.send_message(content, prompt, None) {
+                match client.send_message(content, prompt, Some(0.1)) {
                     Ok(res) => {
-                        println!("\r{}", res.trim());
+                        let mut skin = MadSkin::default();
+                        skin.bold.set_fg(Color::Yellow);
+                        skin.italic.set_fg(Color::Magenta);
+                        skin.code_block.set_fg(Color::White);
+                        skin.inline_code.set_fg(Color::Yellow);
+                        skin.print_text(res.trim());
                     }
                     Err(err) => {
                         eprintln!("\r{:?}", err);
